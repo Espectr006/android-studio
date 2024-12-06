bbdbe29a5c8ac9fe6a12fa7eefd92d1d0cb64717
Miqueias <miqueiasalmeidasantos2000@gmail.com>
2024-12-06 10:10:18 -0300

    hello world

---
+++ app/src/main/java/com/example/projeto02/Cadastro.java
@@ -0,0 +1,155 @@
+package com.example.projeto02;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cadastro extends AppCompatActivity {
+
+    private TextView tela_cadastro;
+    private EditText nomeText, cademail, cadsenha;
+    private Button CadButton;
+    String usuarioID;
+    String[] mensagens = {"Preencha todos os campos","Cadastro realizado com sucesso"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        IniciarComponents();
+
+        tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Cadastro.this, TelaLogin.class);
+                startActivity(intent);
+            }
+        });
+
+        CadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nome = nomeText.getText().toString();
+                String email = cademail.getText().toString();
+                String senha = cadsenha.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    CadastrarUsuario(v);
+                }
+            }
+        });
+    }
+    private void CadastrarUsuario(View v){
+
+        String email = cademail.getText().toString();
+        String senha = cadsenha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    SalvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, mensagens[1],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Digite de um senha com no minimo 6 caracteres";
+                    }catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Este email já foi cadastrado";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "E-mail invalido";
+                    }catch (Exception e){
+                        erro = "Erro ao cadastrar o usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v,erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void SalvarDadosUsuario(){
+        String nome = nomeText.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db","Sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error","Erro ao salvar os dados"+ e.toString());
+            }
+        });
+    }
+    private void IniciarComponents(){
+        nomeText = findViewById(R.id.nomeText);
+        cademail = findViewById(R.id.email);
+        cadsenha = findViewById(R.id.senha);
+        CadButton = findViewById(R.id.CadastroButton);
+        tela_cadastro = findViewById(R.id.Cadtext);
+
+    }
+}
